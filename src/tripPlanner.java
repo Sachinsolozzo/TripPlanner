756e4f17da9d5e1283d2a725a534c3650d8b6901
Sachin <sachinkr0077@gmail.com>
2018-01-28 19:18:49 +0530

    mod2

--- src/tripPlanner.java
+++ src/tripPlanner.java
@@ -38,14 +38,15 @@ public class tripPlanner {
     public static void distanceTime(){
         Scanner input= new Scanner(System.in);
         System.out.println("Please enter the respective longitudinal and latitudinal coordinates (degrees) + of "+start);
-        double lon1= input.nextDouble()*(Math.PI/180);
+        double lon1= input.nextDouble()*(Math.PI/180);   //Converting degrees to radians.
         double lat1= input.nextDouble()*(Math.PI/180);
         System.out.println("Similarly enter the coordinates (degrees) of "+dest+" in the same order");
         double lon2= input.nextDouble()*(Math.PI/180);
         double lat2= input.nextDouble()*(Math.PI/180);
         double dis= 2*6371* Math.asin(Math.sqrt(Math.pow(Math.sin((lat2-lat1)/2),2)+(Math.cos(lat1)*(Math.cos(lat2))*(Math.pow(Math.sin((lon2-lon1)/2),2)))));
+        // The code written above is the Haversine expression to find the Great-Circle Distance between two points.
         System.out.println("Your destination is at a distance of "+dis+" kms.");
-        System.out.println("Ideally it would be a "+(dis/900)+" hr long flight.");
+        System.out.println("Ideally it would be a "+(dis/900)+" hr long flight.");    //average flight speed= 900 kmph
         System.out.println("Thank you for using this program. Have a safe journey.");
     }
 }
