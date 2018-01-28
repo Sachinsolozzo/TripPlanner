2f2f6449b18546c8a3189553eb2ad13cc6e5f8ad
Sachin <sachinkr0077@gmail.com>
2018-01-28 18:01:35 +0530

    mod1

--- src/tripPlanner.java
+++ src/tripPlanner.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
 import java.lang.Math;
 public class tripPlanner {
+    static String start;
+    static String dest;
     public static void main(String[] args) {
+
+        intro();
+        budget();
+        distanceTime();
+
+    }
+    public static void intro(){
         Scanner input= new Scanner(System.in);
         System.out.println("Welcome to the Vacation Planner.");
         System.out.println("Hey there! What's your name?");
         String name= input.nextLine();
         System.out.println("Nice to meet you "+name+", where are you travelling to?");
-        String dest= input.nextLine();
+        dest= input.nextLine();
         System.out.println("Where from?");
-        String start= input.nextLine();
+        start= input.nextLine();
         System.out.println("Great! "+dest+" sounds like a great trip.\n**********");
+    }
+    public static void budget(){
+        Scanner input= new Scanner(System.in);
         System.out.println("\nHow many days are you going to spend travelling?");
         int days= input.nextInt();
         System.out.println("How much money, in rupees are you planning to spend on this trip?");
@@ -21,7 +33,10 @@ public class tripPlanner {
         double convt= input.nextDouble();
         System.out.println("You are travelling for "+days+" days, that is the same as "+(days*24)+" hours or "+(days*24*60)+" minutes.");
         System.out.println("You are going to spend "+money+" rupees, which means you can spend upto "+(money/days)+" rupees per day.");
-        System.out.println("Your total budget in "+symbol+" is "+(money*convt)+" "+symbol+" which is "+(money*convt/days)+" "+symbol+" per day.\n*********");
+        System.out.println("Your total budget in "+symbol+" is "+(money*convt)+" "+symbol+" which is "+(money*convt/days)+" "+symbol+" per day.\n**********");
+    }
+    public static void distanceTime(){
+        Scanner input= new Scanner(System.in);
         System.out.println("Please enter the respective longitudinal and latitudinal coordinates (degrees) + of "+start);
         double lon1= input.nextDouble()*(Math.PI/180);
         double lat1= input.nextDouble()*(Math.PI/180);
@@ -31,7 +46,6 @@ public class tripPlanner {
         double dis= 2*6371* Math.asin(Math.sqrt(Math.pow(Math.sin((lat2-lat1)/2),2)+(Math.cos(lat1)*(Math.cos(lat2))*(Math.pow(Math.sin((lon2-lon1)/2),2)))));
         System.out.println("Your destination is at a distance of "+dis+" kms.");
         System.out.println("Ideally it would be a "+(dis/900)+" hr long flight.");
-        System.out.println("Thankyou for using this program. Have a safe journey.");
-
+        System.out.println("Thank you for using this program. Have a safe journey.");
     }
 }
